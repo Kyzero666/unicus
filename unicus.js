3d9180bbc2f756d8d65060aac3c0b0dac3fd479d
Kyzero666 <38668184+Kyzero666@users.noreply.github.com>
2018-06-06 20:46:25 +0800

    Update unicus.js

--- unicus.js
+++ unicus.js
@@ -1,7 +1,7 @@
 
 const Discord = require('discord.js');
 const unicus = new Discord.Client();
-const weather = require('weather-js');
+//const weather = require('weather-js');
 const prefix = '-';
 let cooldown = new Set();
 let cdsec = 5;
@@ -227,38 +227,38 @@ unicus.on('message', function(message) {
       }
     }
 
-    if (msg.startsWith(prefix + 'WEATHER')) {
+    //if (msg.startsWith(prefix + 'WEATHER')) {
 
 
-        weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-            if (err) message.channel.send(err);
+    //    weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
+    //        if (err) message.channel.send(err);
 
-            if (result === undefined || result.length === 0) {
-                message.channel.send('**Please enter a valid location.**')
-                return;
-            }
+    //        if (result === undefined || result.length === 0) {
+    //            message.channel.send('**Please enter a valid location.**')
+    //            return;
+    //        }
 
             // Variables
-            var current = result[0].current;
-            var location = result[0].location;
+   //         var current = result[0].current;
+    //        var location = result[0].location;
 
 
-            const embed = new Discord.RichEmbed()
-                .setDescription(`**${current.skytext}**`)
-                .setAuthor(`Weather for ${current.observationpoint}`)
-                .setThumbnail(current.imageUrl)
-                .setColor(0x00AE86)
-                .addField('Timezone',`UTC${location.timezone}`, true)
-                .addField('Degree Type',location.degreetype, true)
-                .addField('Temperature',`${current.temperature} Degrees`, true)
-                .addField('Feels Like', `${current.feelslike} Degrees`, true)
-                .addField('Winds',current.winddisplay, true)
-                .addField('Humidity', `${current.humidity}%`, true)
+    //        const embed = new Discord.RichEmbed()
+     //           .setDescription(`**${current.skytext}**`)
+      //          .setAuthor(`Weather for ${current.observationpoint}`)
+       //         .setThumbnail(current.imageUrl)
+        //        .setColor(0x00AE86)
+         //       .addField('Timezone',`UTC${location.timezone}`, true)
+          //      .addField('Degree Type',location.degreetype, true)
+           //     .addField('Temperature',`${current.temperature} Degrees`, true)
+            //    .addField('Feels Like', `${current.feelslike} Degrees`, true)
+             //   .addField('Winds',current.winddisplay, true)
+              //  .addField('Humidity', `${current.humidity}%`, true)
 
 
-                message.channel.send({embed});
-        });
-    }
+          //      message.channel.send({embed});
+      //  });
+   // }
 
 });
 
